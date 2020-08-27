eefb18d6cfa6d1863bd935d445df2263cdd2a785
Javier Melendrez <44476129+javimelendrez@users.noreply.github.com>
2020-08-26 19:25:16 -0700

    function to pick random color
    added a function to pick the random color and all tiles turn to winning color when the correct one is chosen

--- color_game.js
+++ color_game.js
@@ -1,7 +1,8 @@
 //make a list of colors
 var colors = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(255, 255, 255)", "rgb(0, 0, 255)", "rgb(0, 255, 255)", "rgb(0, 255, 0)"];
 console.log("connected");
-var picked_color = colors[3];
+//create a function that will pick a random color and assign it to be picked color
+var picked_color = randomColor();
 var color_display = document.getElementById("colorDisplay");
 var message_display = document.querySelector("#message");
 color_display.textContent = picked_color;
@@ -20,10 +21,27 @@ for(var i = 0; i < squares.length; i++){
         if (clicked_color === picked_color){
             console.log("correct!");
             message_display.textContent = "correct!";
+            //call the change color of squares function
+            change_colors(picked_color);
         }else{
             console.log("incorrect");
             message_display.textContent = "try again!";
             this.style.backgroundColor = "black";
         }
     });
+}
+//create a function to change the colors of all squares given a string color
+function change_colors(color){
+    //loop through the squares and change it to the color of the winning color
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+
+}
+//function to pick a random color
+function randomColor(){
+    //get a random nunmber between the length of colors array, it can not be just straight 3 or 6 because the length is changed depending on the difficulty of the game
+    var random = Math.floor(Math.random() * colors.length);
+    //return the array with the random index
+    return colors[random]; 
 }
